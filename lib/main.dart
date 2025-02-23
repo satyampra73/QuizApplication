2655831ac8a8150ddf2a656dbea0e6a1468e9a3d
Satyam Prajapati <satyam7355@gmail.com>
2025-02-23 13:14:03 +0530

    start screen ui created

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_application/start_screen.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-      
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
-        title: Text(widget.title),
-      ),
-      body: Center(
-        
-        child: Column(
-      
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+  runApp(MaterialApp(
+        debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [const Color.fromARGB(255, 129, 27, 197), const Color.fromARGB(255, 104, 26, 117)]
+          )
         ),
+        child: StartScreen(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-      );
-  }
+    ),
+  ));
 }
---
+++ lib/start_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/quiz-logo.png',
+          width: 300,
+        ),
+
+        SizedBox(height: 20),
+
+        Text('Quiz App',
+        style: TextStyle(
+          fontSize: 25,
+          color: Colors.white
+        ),),
+
+        SizedBox(height: 30),
+
+        OutlinedButton.icon(
+          icon: Icon(Icons.arrow_right_alt,
+          color: Colors.white,
+          ),
+        onPressed: (){
+
+        }, 
+        style: OutlinedButton.styleFrom(
+          backgroundColor: const Color.fromARGB(255, 144, 11, 221),
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20)
+          )
+        ),
+        label: Text(
+          'Start Quiz'
+        ),
+        )
+      ],
+    ));
+  }
+}
