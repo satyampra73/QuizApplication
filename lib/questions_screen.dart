738bc766bff903fac9607e06dd50404200aff4dd
Satyam Prajapati <satyam7355pra@gmail.com>
2025-02-24 18:29:47 +0530

    question screen ui modified

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_application/answer_button.dart';
+import 'package:quiz_application/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -11,31 +12,30 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  final currentQuestion = questions[0];
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('The question...',
-          style: TextStyle(
-            color: Colors.white
-          ),),
-          SizedBox(height: 30),
+      child: Container(
+        margin: EdgeInsets.all(40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(currentQuestion.text, 
+            style: TextStyle(color: Colors.white),
+            textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 30),
       
-        AnswerButton(answerText: "Answer 1", onTap:() {
-
-        }),
-
-        AnswerButton(answerText: "Answer 2", onTap:() {
-
-        }),
-
-        AnswerButton(answerText: "Answer 3", onTap:() {
-
-        }),
-        ],
+            ...currentQuestion.answers.map((answer) {
+              return AnswerButton(answerText: answer, onTap: () {});
+            })
+            
+          ],
+        ),
       ),
     );
   }
