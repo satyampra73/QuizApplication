a436a930a66bcb3807aa16146b7b94f05f528976
Satyam Prajapati <satyam7355pra@gmail.com>
2025-03-07 18:18:39 +0530

    some changes done in question summary

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -10,47 +13,85 @@ class QuestionsSummary extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 300,
+
       child: SingleChildScrollView(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children:
               summaryData.map((data) {
-                return Row(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 43, 133, 46),
-                        shape: BoxShape.circle,
-                      ),
-                      child: Text(
-                        ((data['question_index'] as int) + 1).toString(),
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 43, 133, 46),
+                          shape: BoxShape.circle,
                         ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            data['question'].toString(),
-                            style: TextStyle(
-                              color: const Color.fromARGB(179, 231, 165, 66),
-                              fontSize: 18,
-                            ),
-                            textAlign: TextAlign.right,
+                        child: Text(
+                          ((data['question_index'] as int) + 1).toString(),
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
                           ),
-                          const SizedBox(height: 5),
-                          Text(data['user_answer'].toString()),
-                          Text(data['correct_answer'].toString()),
-                        ],
+                          textAlign: TextAlign.center,
+                        ),
                       ),
-                    ),
-                  ],
+
+                      const SizedBox(width: 10),
+
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              data['question'].toString(),
+                              style: TextStyle(
+                                color: const Color.fromARGB(179, 231, 165, 66),
+                                fontSize: 18,
+                              ),
+                            ),
+                            const SizedBox(height: 5),
+                            Row(
+                              children: [
+                                Text(
+                                  'User Answer : ',
+                                  style: TextStyle(color: Colors.white70),
+                                ),
+                                Expanded(
+                                  child: Text(
+                                    data['user_answer'].toString(),
+                                    style: TextStyle(color: Colors.red),
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  'Correct Answer : ',
+                                  style: TextStyle(color: Colors.white70),
+                                ),
+                                Expanded(
+                                  child: Text(
+                                    data['correct_answer'].toString(),
+                                    style: TextStyle(color: Colors.green),
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 );
               }).toList(),
         ),
